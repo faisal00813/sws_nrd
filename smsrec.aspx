969d0f601b4c14de6592b71f8e4caf62901ca6bb
faisal00813 <faisal00813@gmail.com>
2014-01-20 01:28:23 +0530

    added sms receive facility
    added smsrec.aspx

---
+++ smsrec.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="smsrec.aspx.cs" Inherits="smsrec" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+<asp:SqlDataSource ID="SqlDataSourceGrievance" runat="server"
+     ConnectionString="<%$ ConnectionStrings:SWSDBConnectionString %>" 
+    SelectCommand="SELECT [EmpName], [EmpFHName], [Designation], [Station],
+     [MobileNumber], [Email], [Subject], [EmpRequestLetter] 
+    FROM [GrievanceOnlineRequest]"></asp:SqlDataSource>
+    </form>
+</body>
+</html>
---
+++ smsrec.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class smsrec : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string from = Request.QueryString["from"];
+        string message = Request.QueryString["message"];
+
+        SqlDataSourceGrievance.InsertCommandType = SqlDataSourceCommandType.Text;
+        var insertQuery = "Insert into GrievanceOnlineRequest ([MobileNumber],[Subject]) ";
+        insertQuery += " VALUES (@MobileNumber,@Subject) ";
+        SqlDataSourceGrievance.InsertCommand = insertQuery;
+        SqlDataSourceGrievance.InsertParameters.Add("MobileNumber", from);
+        SqlDataSourceGrievance.InsertParameters.Add("Subject", message);
+        SqlDataSourceGrievance.Insert();
+    }
+}
